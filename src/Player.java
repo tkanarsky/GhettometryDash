92d8b6dd0e6b90d4eb635dd7bf4b2498f60aa650
mayank-sharma-16 <2mayanksharma2@gmail.com>
2017-03-03 14:51:21 -0800

    Began work on player class

---
+++ src/Player.java
@@ -0,0 +1,13 @@
+import com.sun.prism.Image;
+
+public class Player extends GhettoObject{
+
+	
+	private int x, y, width, height;
+	private Image img;
+	
+	public Player(int x, int y){
+		super(x, y);
+	}
+	
+}
