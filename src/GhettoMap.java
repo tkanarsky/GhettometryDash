ae7e7c8dd6939b669e0b6a11c57e1229f737eaec
Timothy Kanarsky <tkanarsky@outlook.com>
2017-03-07 15:02:01 -0800

    Start actual work on the gamemap

--- src/GhettoMap.java
+++ src/GhettoMap.java
@@ -1,5 +1,48 @@
+import java.awt.Dimension;
+import java.io.IOException;
+import java.util.ArrayList;
+
 
 public class GhettoMap {
-	public ObjectType[] mapTemplate;
-	public GhettoObject[] onScreen;
+	public ObjectType[][] mapTemplate;
+	public ArrayList<GhettoObject> map;
+	private Dimension dim;
+	private int rows;
+	private int columns;
+	private int templateColumnToLoad = 0;
+	private static final int BLOCK_WIDTH = 60;
+	private boolean gameOver = false;
+	
+	public GhettoMap(Dimension dim, String levelName) {
+		try {
+			mapTemplate = new BitmapParser(levelName).readBitmap();
+		} catch (IOException e) {
+			Log.log("Level file not found", Log.ERROR);		
+			e.printStackTrace();
+		}
+		this.dim = dim;
+		loadObjects();
+	}
+	
+	private void loadObjects() {
+		for (int y = 0; y < mapTemplate.length; y++) { 
+			for (int x = 0; x < mapTemplate[x].length; x++) {
+				switch(mapTemplate[y][x]) {
+				case EMPTY:
+					break;
+				case SPIKE:
+					map.add(new Spike(x*BLOCK_WIDTH, y*BLOCK_WIDTH));
+				}
+				
+				}
+			}
+		}
+	}
+
+	private void advancePlayer() {
+		if (!gameOver) {
+			play
+		}
+	
+	
 }
--- src/Player.java
+++ src/Player.java
@@ -5,61 +5,13 @@ import java.net.URL;
 
 import javax.imageio.ImageIO;
 
-public class Player {
+public class Player extends GhettoObject{
 
-	
-	private int x, y, width, height, speed;
-	private Vec2 direction;
-	private Image img;
-	
-	public Player(int x, int y){
-		//super(x, y);
-		this.x = x;
-		this.y = y;
-		try
-		{
-			URL url = getClass().getResource("res/basicSquare.png");
-			img = ImageIO.read(url);
-		}
-		catch (IOException e)
-		{
-			System.out.println("Problem opening the .jpg");
-			e.printStackTrace();
-		}
+	public Player(int x, int y) {
+		super(x, y, 60, 60, "basicSquare");
 		
 	}
 	
-	public int getX(){
-		return x;
-	}
 	
-	public int getY(){
-		return y;
-	}
 	
-	public void setX(int x){
-		this.x = x;
-	}
-	
-	public void setY(int y){
-		this.y = y;
-	}
-	
-	public void draw(Graphics g){
-		g.drawImage(img, x, y, 20, 20, null);
-	}
-	
-	public void move(){
-		x += 5;
-	}
-	
-	public void setMoving(Vec2 direction, int speed){
-		this.direction = direction;
-		this.speed = speed;
-		
-	}
-	
-	protected void onCollide(LevelObject LO){
-		
-	}
 }
