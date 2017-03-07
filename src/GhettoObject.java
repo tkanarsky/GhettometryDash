cddc6a0336b7fc9462a0cf7bd8e57b74ea26f41f
Timothy Kanarsky <tkanarsky@outlook.com>
2017-03-07 14:27:29 -0800

    Clean up GhettoObject to better suit our purposes

--- src/GhettoObject.java
+++ src/GhettoObject.java
@@ -4,69 +4,48 @@ import java.awt.Image;
 import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.net.URL;
 import java.awt.Graphics2D;
 
 import javax.imageio.ImageIO;
 
 public class GhettoObject {
-	protected int speed, x, y, width, height, health, direction;
-	public String type;
+	protected int x, y, width, height;
 	public static final int NORTH = 1, SOUTH = 3, EAST = 2, WEST = 4;
+	protected BufferedImage img;
+	protected Rectangle boundingRect;
 
-	public GhettoObject(int inputx, int inputy, int inputwidth, int inputheight, String inputtype) {
+	public GhettoObject(int inputx, int inputy, int inputwidth, int inputheight, String imageName) {
 		x = inputx;
 		y = inputy;
 		width = inputwidth;
 		height = inputheight;
-		type = inputtype;
+		loadImage(imageName);
 	}
 
 	public void draw(Graphics g) {
+		g.drawImage(img, x, y, null);
+	}
+	
+	protected void loadImage(String imageName) {
 		try {
-			BufferedImage img = ImageIO.read(getClass().getResourceAsStream(imageFileName()));
-
-			img = (BufferedImage) getScaledImage(img, width, height);
-
-			g.drawImage(img, x, y, null);
-
-		} catch (IOException e) {
+			URL url = getClass().getResource("res/" + imageName + ".png");
+			img = ImageIO.read(url);
+		} catch (Exception e) {
+			System.out.println("Image could not be opened: " + "res/" + imageName + ".png");
 			e.printStackTrace();
 		}
+		img = getScaledImage(img, width, height);
 	}
 
-	private String imageFileName() {
-		return null;
-	}
-
-	public Image getScaledImage(Image srcImg, int w, int h) {
-		//resizes the image
+	public BufferedImage getScaledImage(Image srcImg, int w, int h) {
 		BufferedImage resizedImg = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
 		Graphics2D g2 = resizedImg.createGraphics();
-		/* System.out.println("meep");
-		g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
-		RenderingHints.VALUE_INTERPOLATION_BILINEAR); */
 		g2.drawImage(srcImg, 0, 0, w, h, null);
 		g2.dispose();
-
 		return resizedImg;
 	}
-
-	public void setType(String input) {
-		type = input;
-	}
-
-	public String getType() {
-		return type;
-	}
-
-	public void setSpeed(int input) {
-		speed = input;
-	}
-
-	public int getSpeed() {
-		return speed;
-	}
-
+	
 	public void setX(int input) {
 		x = input;
 	}
@@ -98,57 +77,10 @@ public class GhettoObject {
 	public int getHeight() {
 		return height;
 	}
-
-	public void setHealth(int input) {
-		health = input;
-	}
-
-	public int getHealth() {
-		return health;
-	}
-
-	public void setLevel(int input) {
-		level = input;
-	}
-
-	public int getLevel() {
-		return level;
-	}
-
-	public void setDirection(int input){
-		direction = input;
-	}
 	
-	public int getDirection(){
-		return direction;
+	public Rectangle getBoundingBox() {
+		return new Rectangle(x, y, width, height);
 	}
-	
-	
-	@Override
-	public void move() {
-		//moves with boundaries based on speed, if the next move is off the map, it will not move
-		if (x <= (600-width-speed) && direction == EAST) {
-			setX(x + speed);
-		} else if (x >= speed-1 && direction == WEST) {
-			setX(x - speed);
-//			System.out.println(x + " " + y + " " + speed);
-		} else if (y >= speed && direction == NORTH) {
-			setY(y - speed);
-		} else if (y <= (600-height-speed) && direction == SOUTH) {
-			setY(y + speed);
-		}
-	}
-
-	public void nameYoSelf() {
-		//i just did this for fun, apparently it's supposed to be called "toString"
-		System.out.println(type + " at " + x + " " + y);
-	}
-
-
-	@Override
-	public Rectangle getBoundingRect() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		
 
 }
